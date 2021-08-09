28ffba3ebe1e8180915732aff37cd5b8b71840f6
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-08-09 12:47:21 +0100

    refactoring main function for part 2 into separate functions

--- part2_annotation/scripts/analyse_warnings.py
+++ part2_annotation/scripts/analyse_warnings.py
@@ -130,12 +130,13 @@ def match_line(filename,start,stop):
 
 
 def part1_analysis(vep_folder, outfolder):
-    smple_list=[]
+    
     p=Path(vep_folder)
     #Get all warning file names
     warning_files=list(p.glob('*_warnings.txt'))
     sample_list=get_sample_information_from_vep_files(warning_files)
     part1_print_to_file(outfolder, sample_list)
+    return sample_list
 
 def get_sample_information_from_vep_files(warning_files_list):
     sample_list=[]
@@ -159,37 +160,55 @@ def part1_print_to_file(output_file, samples):
                 outf.write(f"{s.sampleID}\t{w.warning_type}\t{w.warning}\n")
 
 
-def main(args):
-    sample_list=[]
-    part1_analysis(args.vep_folder, args.warnings_output_file1)
-    
-
-    
-    '''
-    Part 2
-    '''
-    #Get all the dias_extract files from folder
-    dias=Path(args.dias_folder)
+def part2_analysis(samples, dias_folder, output_file):
+    dias=Path(dias_folder)
     dias_files=list(dias.glob('*.tsv'))
+    #Associate each dias_extract file with its Sample object that it belongs to
+    samples_with_dias_info=get_sample_info_from_dias_files(samples, dias_files)
+    #Find matching line in dias_extract file that refers to the VEP_output input warning for that Sample
+    samples_with_problem_line=find_problem_line_from_dias_files(samples_with_dias_info)
+    #Print to output file
+    part2_print_to_file(output_file, samples_with_problem_line)
+
+
+
+def get_sample_info_from_dias_files(sample_list, dias_files):
     #Associate each dias_extract file with its Sample object that it belongs to
     for f in dias_files:
         sampleID=extract_sample_name(f)
         for sample in sample_list:
             if sample.sampleID == sampleID:
                 sample.dias_path=f
-    #Find matching line in dias_extract file that refers to the VEP_output input warning for that Sample
-    for s1 in sample_list:
-        for w in s1.warnings:
+    return sample_list
+
+def find_problem_line_from_dias_files(sample_list):
+    for s in sample_list:
+        for w in s.warnings:
             if w.warning_type == INPUT_WARNING:
-                w.problem_line=match_line(s1.dias_path, w.start, w.stop)
-    with open(args.warnings_output_file2,'w') as outf:
+                w.problem_line=match_line(s.dias_path, w.start, w.stop)
+    return sample_list
+
+def part2_print_to_file(output_file,samples):
+    with open(output_file,'w') as outf:
         outf.write(f'Sample\tWarning_from_VEP_output\tCorresponding_Line_from_Dias_extract_file\tVep_output_warning_file\tDias_extract_file\n')
-        for s1 in sample_list:
-            for w in s1.warnings:
+        for s in samples:
+            for w in s.warnings:
                 if w.warning_type == INPUT_WARNING:
-                    outf.write(f"{s1.sampleID}\t{w.warning}\t{w.problem_line}\t{s1.vep_out_path}\t{s1.dias_path}\n")
+                    outf.write(f"{s.sampleID}\t{w.warning}\t{w.problem_line}\t{s.vep_out_path}\t{s.dias_path}\n")
 
 
+
+def main(args):
+    '''
+    Part 1
+    '''
+    samples_with_warnings=part1_analysis(args.vep_folder, args.warnings_output_file1)
+    
+    
+    '''
+    Part 2
+    '''
+    part2_analysis(samples_with_warnings, args.dias_folder, args.warnings_output_file2)
 
 
 if __name__ =="__main__":
